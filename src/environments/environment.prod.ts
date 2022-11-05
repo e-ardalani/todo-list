7a553701e6c5a48b33b800ae23a73f4bbf3b778b
eli <elnaz.ardalani6626@gmail.com>
2022-11-05 12:32:27 +0330

    create base project

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyChpVgEdPdvb9ExCaUOl_v1osPIAhVEAvk',
+    authDomain: 'todolist-e8e62.firebaseapp.com',
+    databaseURL: 'https://todolist-e8e62-default-rtdb.firebaseio.com/null',
+    projectId: 'todolist-e8e62',
+    storageBucket: 'todolist-e8e62.appspot.com',
+    messagingSenderId: '369400283382'
+  }
+};
