f43e40228a0715ef73176feb53aa928c06ec1f4b
eli <elnaz.ardalani6626@gmail.com>
2022-11-07 03:37:34 +0330

    add i18n to signup component

--- src/app/pages/auth/components/sign-up/sign-up.component.ts
+++ src/app/pages/auth/components/sign-up/sign-up.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../services/auth.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,11 +8,27 @@ import {AuthService} from '../../services/auth.service';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
+  formGroup: FormGroup;
 
   constructor(public authService: AuthService) {
   }
 
   ngOnInit(): void {
+    this.createForm();
   }
+
+  createForm() {
+    this.formGroup = new FormGroup({
+      firstName: new FormControl(null, [Validators.required]),
+      lastName: new FormControl(null, [Validators.required]),
+      userName: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required])
+    });
+  }
+
+  signUp(form) {
+    this.authService.SignUp(form.firstName, form.lastName, form.userName, form.password).finally();
+  }
+
 
 }
